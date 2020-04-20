55b6006f651cea3c7c1986dcd4f18d4fb42a0a3b
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-20 05:27:16 +0300

    hede

--- vars/parseAngularJson.groovy
+++ vars/parseAngularJson.groovy
@@ -41,6 +41,10 @@ def  Map<String, Paket> call(String file) {
             println "WORKSPACE: ${WORKSPACE}"
             def dir = "${WORKSPACE}/developer/package.json"
             println "dirrrrrrrrrrrr: ${dir}"
+            def packageJsonPath = "./developer/${root}/package.json"
+            println "packageJsonPath: ${packageJsonPath}"
+            def packageJsonLines = readFile(file: packageJsonPath)
+            println "packageJsonLines: ${packageJsonLines}"
             res.put(name, new Paket(name, root, parsePackageJson(dir)))
         }
     }
