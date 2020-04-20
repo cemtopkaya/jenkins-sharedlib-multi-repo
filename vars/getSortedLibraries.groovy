d6258dfe1a5c5adb7c6ffb3bda034315bd5a6bf4
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-20 11:52:01 +0300

    hede

--- vars/getSortedLibraries.groovy
+++ vars/getSortedLibraries.groovy
@@ -16,8 +16,9 @@ def call(Map<String, Paket> param){
             //println "   a.dependencies: ${param.get(a).dependencies}"
             //println "   b.dependencies: ${param.get(b).dependencies}"
             //println "   a.dep.contains(b): ${param.get(a).dependencies?.contains(b)}"
-            //println "a: $a -- b: $b -- \n\t a.dependencies: ${param.get(a).dependencies}"
             
+            //println "a: $a -- b: $b -- \n\t a.dependencies: ${param.get(a).dependencies}"
+
             if(param.get(a).dependencies == null) return -1
             if(param.get(a).dependencies.contains(b)) return 1
             if(param.get(b).dependencies.contains(a)) return -1
