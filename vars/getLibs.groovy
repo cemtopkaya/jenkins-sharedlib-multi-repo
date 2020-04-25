cc7bc44b92c27205b59652667c9f81b6fa87ad25
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-25 18:44:53 +0300

    hede

--- vars/getLibs.groovy
+++ vars/getLibs.groovy
@@ -16,23 +16,8 @@ def Map<String, Paket> call(String prjDirPath) {
     
     def res = [:]
     String pathAngularJson = "$prjDirPath/angular.json"
-    // println "pathAngularJson: $pathAngularJson"
-
-    // sh "ls -l"
     
     try{
-        println ">>> angular.json var mi?"
-        def varmi = fileExists pathAngularJson
-        println varmi
-
-//         def contentOfAngularJson = sh(
-//                         label: "Angular.json oku",
-//                         returnStdout: true, 
-//                         script: "cat $pathAngularJson"
-//                     ).trim()
-// println contentOfAngularJson
-
-//         def jsn = readJSON  text: contentOfAngularJson, returnPojo: true
         def jsn = readJSON file: pathAngularJson
         jsn["projects"].each { k, v ->
             println "---------- $k --------------"
@@ -40,11 +25,7 @@ def Map<String, Paket> call(String prjDirPath) {
                 res.put(k, new Paket(k, jsn["projects"][k]["root"], []))
             }
         }
-        println ":::::::.res::::::"
-        println res
-        println res.getClass()
-        params.RES = res
-        println params.RES.getClass()
+
         return res
 
     }catch(err){
