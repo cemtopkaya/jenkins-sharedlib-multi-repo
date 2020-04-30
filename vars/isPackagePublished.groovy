8d1aff4168a7964319b88b1b0a9d4d6d370249fd
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-30 18:49:43 +0300

    hede

--- vars/isPackagePublished.groovy
+++ vars/isPackagePublished.groovy
@@ -15,7 +15,7 @@ def call(String registry, String pgk, String version){
             label: "REST sorgusuyla verdaccio kontrol ediliyor: $script",
             returnStatus: true,
             script: script
-        )
+        ).toBoolean()
     
         echo "--->>> is published - Version say?s?: $isExist"
 
