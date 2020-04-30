a2de777a99119497584099252d856afe5c4ca8d0
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-30 19:08:44 +0300

    hede

--- vars/isPackagePublished.groovy
+++ vars/isPackagePublished.groovy
@@ -9,7 +9,7 @@ def call(String registry, String pgk, String version){
     try {
         println ">>> registry: $registry , pgk: $pgk , version: $version "
         // sh "curl -s http://localhost:4873/@kapsam2/kutup11 | awk '/0.0.1/{count++;} END{print count=="" ? 0 : count}'"
-        String script = "curl -s $registry/$pgk | awk '/$version/{count++;} END{isExist = (length(count)>0); print isExist}'"
+        String script = "curl -s $registry/$pgk | awk '/version.*:.*$version/{count++;} END{isExist = (length(count)>0); print isExist}'"
         println "script: $script"
         String output = sh (
             label: "REST sorgusuyla verdaccio kontrol ediliyor: $script",
