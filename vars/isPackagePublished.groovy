010ef0067a3135d5b99c61e240e9f87fbf58a6f0
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-30 18:53:10 +0300

    hede

--- vars/isPackagePublished.groovy
+++ vars/isPackagePublished.groovy
@@ -11,11 +11,13 @@ def call(String registry, String pgk, String version){
         // sh "curl -s http://localhost:4873/@kapsam2/kutup11 | awk '/0.0.1/{count++;} END{print count=="" ? 0 : count}'"
         String script = "curl -s $registry/$pgk | awk '/$version/{count++;} END{isExist = (length(count)>0); print isExist}'"
         println "script: $script"
-        Boolean isExist = ( 1== sh (
+        def output = sh (
             label: "REST sorgusuyla verdaccio kontrol ediliyor: $script",
             returnStatus: true,
             script: script
-        ))
+        )
+        println output.class.name
+        Boolean isExist = ( 1== )
     
         echo "--->>> is published - Version say?s?: "+isExist
 
