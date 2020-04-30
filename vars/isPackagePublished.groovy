a4f50877f9f995795745dcf80f5e825f76c420a3
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-30 19:16:35 +0300

    hede

--- vars/isPackagePublished.groovy
+++ vars/isPackagePublished.groovy
@@ -4,6 +4,11 @@
  */
 
 def call(String registry, String pgk, String version){
+    // TODO: Check if curl exists try fromApi otherwise fromNpmView
+    return fromApi(registry, pgk, version)
+}
+
+def fromApi(String registry, String pgk, String version){
     println "----------------- isPackagePublished -------------------"
     
     try {
@@ -27,4 +32,32 @@ def call(String registry, String pgk, String version){
         throw err
     }
 
+}
+
+def fromNpmView(String registry, String pgk, String version){
+    npmViewScript = "npm view ${pgk}@${version} ${registry}"
+    echo "** Ayn? versiyon kullan?lm?? m? kontrol? i?in script > npmViewScript: ${npmViewScript}"
+    /* E?er npm view aranan paketi bulamazsa sh komutu 1 (Error 404) ile hata f?rlatarak ??kacak!
+    * Bu y?zden kod k?r?lmas?n diye "returnStatus: true" ile sh execute edilecek ve exit code okunacak.
+    * Exit code 0'dan farkl?ysa ise yani "npm ERR! code E404" ile npm view hata f?rlatarak ??k?? yapm??sa bilece?iz ki; paket yok!
+    * E?er normal ??k?? yapm??sa bu kez ??kt?y? almak i?in "returnStdout: true" anahtar?yla tekrar paket sorgulanacak
+    **/
+    
+    npmViewStatusCode = sh(returnStatus: true, script: "$npmViewScript")
+    if(npmViewStatusCode == 1){
+        result = false
+    }else {
+        try {
+            checkVersionPublished = sh(
+                label: "$npmViewScript",
+                returnStdout: true, 
+                script: "${npmViewScript} | wc -m"
+            ).trim() as Integer
+            
+            result = checkVersionPublished > 0
+        }
+        catch (err) {
+            println "npm view hata f?rlatt?: $err"
+        }
+    }
 }
