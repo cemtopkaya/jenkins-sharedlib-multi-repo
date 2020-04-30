f6cb99c08bf992cebe146e7578f0d4445ccb630e
Cem Topkaya <cem.topkaya@hotmail.com>
2020-04-30 18:55:36 +0300

    hede

--- vars/isPackagePublished.groovy
+++ vars/isPackagePublished.groovy
@@ -17,7 +17,7 @@ def call(String registry, String pgk, String version){
             script: script
         )
         println "'"+output+"'"
-        println output>0
+        println output as Integer > 0 as Integer
         println output.class.name
         Boolean isExist = ( 1== output)
     
