d21e42619b649ecc27feebbc3bc6d291b5d4a862
Cem Topkaya <cem.topkaya@hotmail.com>
2020-05-02 17:15:16 +0300

    hede

--- src/ulak/Node/NpmPackage.groovy
+++ src/ulak/Node/NpmPackage.groovy
@@ -127,7 +127,7 @@ class NpmPackage{
     static def setNpmConfigRegistries(def ctx, Map<String,String> scopeRegistries, Boolean isGlobal=false){
         println "----------------- setNpmConfigRegistries -------------------"
         
-        println "ctx: $ctx"
+        println ">>> pwd: ${pwd()}"
         
         try {
             scopeRegistries.each{
